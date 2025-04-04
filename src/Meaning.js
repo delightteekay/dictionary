7ec527a06815540e6af305ba53348e476ade0aa6
delightteekay <162933257+delightteekay@users.noreply.github.com>
2025-04-05 00:28:51 +0200

    added phonetic styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 
 export default function Meaning(props) {
@@ -10,16 +11,19 @@ export default function Meaning(props) {
         {props.meaning.definitions.map(function(definition, index) {
             return (
                 <div key={index}>
-                <p>
-                <strong>Definition:</strong> {definition.definition}
-                <br />
-                <strong>Example:</strong>
-                <em>{definition.example}</em>
+                <div className="definition">
+                {definition.definition}
+                </div>
+                <div className="example">
+               <em>{definition.example}</em>
                 <Synonyms synonyms={definition.synonyms} />
-                </p>
                 </div>
             );
         })}
         </div>
+        }
+        
     );
+
+    
 }
